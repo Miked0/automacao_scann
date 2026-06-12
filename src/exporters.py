2939cfa76f21937cf8137f5e487e330ba13fe9b7
Michael Douglas <102732673+Miked0@users.noreply.github.com>
2026-06-12 13:48:52 -0300

    Delete src/exporters.py

--- src/exporters.py
+++
@@ -1,4 +0,0 @@
-# Este arquivo foi renomeado para exportadores.py
-# Importe a partir do módulo correto:
-#   from exportadores import exportar_resultados
-raise ImportError("Use 'from exportadores import exportar_resultados' no lugar deste módulo.")
