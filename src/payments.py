1bb9ea8110bf6d94f17a0932d64f7b422148333f
Michael Douglas <102732673+Miked0@users.noreply.github.com>
2026-06-12 13:48:54 -0300

    Delete src/payments.py

--- src/payments.py
+++
@@ -1,4 +0,0 @@
-# Este arquivo foi renomeado para pagamentos.py
-# Importe a partir do módulo correto:
-#   from pagamentos import normalizar_pagamento
-raise ImportError("Use 'from pagamentos import normalizar_pagamento' no lugar deste módulo.")
