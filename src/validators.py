27c139760c18cf541eb2a558d28d78b7251d91a7
Michael Douglas <102732673+Miked0@users.noreply.github.com>
2026-06-12 13:48:50 -0300

    Delete src/validators.py

--- src/validators.py
+++
@@ -1,4 +0,0 @@
-# Este arquivo foi renomeado para validadores.py
-# Importe a partir do módulo correto:
-#   from validadores import validar_caso_de_teste
-raise ImportError("Use 'from validadores import validar_caso_de_teste' no lugar deste módulo.")
