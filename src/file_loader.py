b087e428eddb98d41360aef1504a63136fcba245
Michael Douglas <102732673+Miked0@users.noreply.github.com>
2026-06-12 13:48:52 -0300

    Delete src/file_loader.py

--- src/file_loader.py
+++
@@ -1,4 +0,0 @@
-# Este arquivo foi renomeado para carregador_arquivos.py
-# Importe a partir do módulo correto:
-#   from carregador_arquivos import CarregadorArquivos
-raise ImportError("Use 'from carregador_arquivos import CarregadorArquivos' no lugar deste módulo.")
